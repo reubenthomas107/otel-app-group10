a1a2ef35a56e534895049a0b137c5e6b6361d8a9
reubenthomas107 <reubenthomas107@gmail.com>
2025-04-29 17:38:18 -0400

    error fix

--- k8s/deploy_k8s_manifest.sh
+++ k8s/deploy_k8s_manifest.sh
@@ -5,4 +5,4 @@ K8S_NAMESPACE="otel-demo"
 cd /home/ec2-user
 git clone https://github.com/open-telemetry/opentelemetry-demo.git
 
-kubectl create --namespace $K8S_NAMESPACE -f /opentelemetry-demo/kubernetes/opentelemetry-demo.yaml
+kubectl create --namespace $K8S_NAMESPACE -f opentelemetry-demo/kubernetes/opentelemetry-demo.yaml
